cec28e13a0377a4b7fcb5d446d48f5c790480c12
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2025-12-17 15:56:31 +0530

    [Harsh Raj Singh] : added a program to convert Kilometer to miles

---
+++ gcr-codebase/programming-elements/Level-1/KMToMiles.java
@@ -0,0 +1,11 @@
+public class KMToMiles{
+    public static void main(String [] args){
+	    
+        double DISTANCE=10.8; // in kilometers
+        // converting distance from kilometers to miles
+        double NEW_DISTANCE= 10.8 * 1.6;
+
+        // Printing the converted Distance
+        System.out.println("The distance " + DISTANCE+ " km in miles is "+ NEW_DISTANCE);		
+	}
+}
