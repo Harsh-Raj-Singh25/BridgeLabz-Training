94369a619d60b35f431d8ec231f51a3344395688
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2025-12-17 12:13:37 +0530

    [Harsh Raj Singh] : Created a sum program

---
+++ gcr-codebase/prerequisites/sum.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class sum{
+public static void main(String[] args){
+ Scanner hrs=new Scanner(System.in);
+ int a=hrs.nextInt();
+ int b=hrs.nextInt();
+ System.out.println(a+b);
+}
+}
