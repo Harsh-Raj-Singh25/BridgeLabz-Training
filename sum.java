f5b45e5484d9758b2e6328b3ea7b9cd698b099c7
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2025-12-17 11:54:45 +0530

    [Harsh Raj Singh] : Clearing the folder structure

--- sum.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class sum{
-public static void main(String[] args){
- Scanner hrs=new Scanner(System.in);
- int a=hrs.nextInt();
- int b=hrs.nextInt();
- System.out.println(a+b);
-}
-}
