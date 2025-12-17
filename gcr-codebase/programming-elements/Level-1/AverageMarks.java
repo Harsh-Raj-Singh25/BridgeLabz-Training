bd63caa365c46518acb9c86e2120fa4478b7064c
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2025-12-17 15:31:16 +0530

    [Harsh Raj Singh] : created program AverageMarks

---
+++ gcr-codebase/programming-elements/Level-1/AverageMarks.java
@@ -0,0 +1,17 @@
+public class AverageMarks{
+    public static void main(String [] args){
+		
+		// marks in maths
+		int MATH=94;
+		// marks in physics
+		int PHYSICS=95;
+		// marks in chemistry
+		int CHEMISTRY=96;
+		
+	    // Average percent mark in PCM
+        int AVERAGE=(94 + 95 + 96)/3;
+		
+		// Print the average marks
+		System.out.println("Sam’s average mark in PCM is : "+ AVERAGE);
+	}
+}
