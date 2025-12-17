a5f247a4eab53db793ca5cf494500351b818740b
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2025-12-17 15:20:59 +0530

    [Harsh Raj Singh] : added a new folder level-1

---
+++ gcr-codebase/programming-elements/Level-1/AgeHarry.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AgeHarry{
+    public static void main(String [] args){
+	
+		int BIRTH_YEAR=2000;
+		int CURRENT_Year=2024;
+		// calculating age
+		int AGE= CURRENT_Year - BIRTH_YEAR;
+		//Print the age
+		System.out.println("Harry's age in " + CURRENT_Year + " is "+AGE);
+	}
+}
+		
