f1ab9fc94512f05341c3ce5cbc697b0e51e3312a
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2025-12-18 17:11:49 +0530

    [Harsh Raj Singh] : added a program FactorialFor to level-1

---
+++ gcr-codebase/control-flow/Level-1/FactorialFor.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class FactorialFor{
+	public static void main(String [] args){
+		Scanner s=new Scanner(System.in);
+		
+		//Taking input from the user
+        System.out.println("Enter your number");
+		int number=s.nextInt();
+		// initialize the factorial as fact=1;
+		int fact=1;
+		// calculating the factorial using while loop
+		for(int i=number;i>0;i--){
+			fact*=i;
+		}
+		System.out.println("The factorial is : "+ fact);
+			
+		
+		
+	}
+}
