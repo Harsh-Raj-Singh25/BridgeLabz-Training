910297955750ad21f5abff9fd71e7e1e5987c32e
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2025-12-19 00:35:49 +0530

    [Harsh Raj Singh] : added a new program

---
+++ gcr-codebase/control-flow/Level-3/LeapYearmodified.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LeapYearmodified {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // take year as input
+        System.out.println("Enter your year");
+        int year = sc.nextInt();
+        // check for year 1582 factor and if not less than 1582 then proceed further
+        if (year < 1582) {
+            System.out.println("Invalid year : due to 1582 factor");
+        } else {
+            // check for leap year
+            if ((year %400==0) || ((year % 4==0) && (year % 100!=0))){
+                System.out.println("The year "+ year+" is a leap year");
+            }else{
+                System.out.println("The year "+ year+" is not a leap year");
+            }
+        }
+        sc.close();
+    }
+}
