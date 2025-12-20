04c1d02dd1abaeb1105c5e2347e040033c157f46
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2025-12-20 12:13:54 +0530

    [Harsh Raj Singh] : added LargestSecondLargestDigitModified program

---
+++ gcr-codebase/arrays/Level-2/LargestSecondLargestDigitModified.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class LargestSecondLargestDigitModified{
+	public static void main(String args[]){
+
+        Scanner input = new Scanner(System.in);
+        // Take user input
+        int number = input.nextInt();
+        
+		int maxDigit = 10;
+        int[] digits = new int[maxDigit];
+        int index = 0;
+
+
+        // Store digits in array
+        while(number != 0){
+		// Increase array size if limit reached
+            if(index == maxDigit){
+                maxDigit = maxDigit + 10;
+                int[] temp = new int[maxDigit];
+
+                // Copy old digits to new array
+                for(int i = 0 ; i < digits.length ; i++){
+                    temp[i] = digits[i];
+                }
+
+                digits = temp;
+            }
+			digits[index] = number % 10;
+            number = number / 10;
+            index++;
+        }
+
+        int largest = 0;
+        int secondLargest = 0;
+
+        // Find largest and second largest digit
+        for(int i = 0 ; i < index ; i++){
+            if(digits[i] > largest){
+                secondLargest = largest;
+                largest = digits[i];
+            }else if(digits[i] > secondLargest && digits[i] != largest){
+                secondLargest = digits[i];
+            }
+        }
+
+        
+
+		// Display result
+        System.out.println("Largest digit: " + largest);
+        System.out.println("Second largest digit: " + secondLargest);
+
+
+        input.close();
+    }
+}
