f81d8b4f9d719d28612cf8ebfd31ff15d84da72e
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2025-12-24 08:56:23 +0530

    [Harsh Raj Singh] : added a new program

---
+++ gcr-codebase/Strings/Level-2/VowelConsonant.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class VowelConsonant{
+    // character is a vowel, consonant, or not a letter
+    public static String checkCharacter(char ch) {
+        ch = Character.toLowerCase(ch);
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return "Vowel";
+            } else {
+                return "Consonant";
+            }
+        } else {
+            return "Not a Letter";
+        }
+    }
+    // find vowels and consonants
+    public static int[] findVowelsAndConsonants(String str) {
+        int vowelCount = 0;
+        int consonantCount = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String result = checkCharacter(ch);
+            if (result.equals("Vowel")) {
+                vowelCount++;
+            } else if (result.equals("Consonant")) {
+                consonantCount++;
+            }
+        }
+        return new int[]{vowelCount, consonantCount};
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		// take user input
+        System.out.print("string: ");
+        String s = scanner.nextLine();
+        int[] counts = findVowelsAndConsonants(s);
+        System.out.println("Vowels: " + counts[0]);
+        System.out.println("Consonants: " + counts[1]);
+
+        scanner.close();
+    }
+}
