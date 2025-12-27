53f1c63528095161b6394726b0e98df365cc6d0d
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2025-12-27 23:40:42 +0530

    [Harsh Raj Singh] : added new program as subclass to student class file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/PostgraduateStudent.java
@@ -0,0 +1,11 @@
+package com.constructor.level1;
+
+public class PostgraduateStudent {
+
+	public static void main(String[] args) {
+		
+		Student s1=new Student("0111Al221078","Harsh",8.9);
+		s1.displayDetails();
+	}
+
+}
