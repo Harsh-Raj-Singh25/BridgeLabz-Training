2debf29a14800c7d63764421528f7bebb604f5e2
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2025-12-31 15:32:56 +0530

    [Harsh Raj Singh] : added a new package BankAccount showing association between bank and customers

--- java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/objectModeling/level1/bankAccount/BankAccountMain.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/objectModeling/level1/bankAccount/BankAccountMain.java
@@ -3,18 +3,17 @@ package com.objectModeling.level1.bankAccount;
 public class BankAccountMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Bank hdfc = new Bank("HDFC Bank");
         Customer customer1 = new Customer("Harsh Singh", 5000.0);
 
-        // Step 2: Establish association through communication
+        // Establish association through communication
         // The Bank object acts upon the Customer object
         hdfc.openAccount(customer1);
 
-        // Step 3: Customer performs an action independently
+        //Customer performs an action independently
         customer1.viewBalance();
 
-        // Step 4: Another communication example
+        // Another communication example
         hdfc.deposit(customer1, 2000.0);
 	}
 
