f42b31fe7cdd25c7d13ee7051837a580266ff9bf
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-01-01 17:00:49 +0530

    [Harsh Raj Singh]: added a new folder showing Hierarchical inheritance

--- java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/Inheritance/Hierarchical_Inheritance/BankAccountTypes/Main.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/Inheritance/Hierarchical_Inheritance/BankAccountTypes/Main.java
@@ -2,9 +2,11 @@ package com.Inheritance.Hierarchical_Inheritance.BankAccountTypes;
 
 public class Main {
 	public static void main(String[] args) {
+		// making objects
         SavingsAccount sa = new SavingsAccount("SAV123", 50000, 4.5);
         CheckingAccount ca = new CheckingAccount("CHK456", 20000, 10000);
         
+        // using methods
         sa.displayAccountType();
         System.out.println("----------------------------------------------");
         ca.displayAccountType();
