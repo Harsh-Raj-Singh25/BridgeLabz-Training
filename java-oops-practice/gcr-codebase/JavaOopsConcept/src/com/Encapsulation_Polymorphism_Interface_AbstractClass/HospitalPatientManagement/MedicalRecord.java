4587ddb9a30a0efd66c05a2401a1fd018dd009af
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-01-02 12:28:15 +0530

    [Harsh Raj Singh] : added a new file denoting Medical Record class in Hospital Patient Management System

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/Encapsulation_Polymorphism_Interface_AbstractClass/HospitalPatientManagement/MedicalRecord.java
@@ -0,0 +1,7 @@
+package com.Encapsulation_Polymorphism_Interface_AbstractClass.HospitalPatientManagement;
+
+//INTERFACE: Ensures medical data handling standards
+interface MedicalRecord {
+	void addRecord(String entry);
+	void viewRecords();
+}
