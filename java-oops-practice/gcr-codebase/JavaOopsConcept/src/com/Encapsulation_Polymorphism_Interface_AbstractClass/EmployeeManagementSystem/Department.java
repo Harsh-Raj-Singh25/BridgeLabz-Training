4e9ae166ffc91007945e3af1f18be7bbea54323f
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-01-02 08:50:33 +0530

    [Harsh Raj Singh] : added a new file denoting Department class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/Encapsulation_Polymorphism_Interface_AbstractClass/EmployeeManagementSystem/Department.java
@@ -0,0 +1,6 @@
+package com.Encapsulation_Polymorphism_Interface_AbstractClass.EmployeeManagementSystem;
+
+public interface Department {
+	void assignDepartment(String deptName);
+    String getDepartmentDetails();
+}
