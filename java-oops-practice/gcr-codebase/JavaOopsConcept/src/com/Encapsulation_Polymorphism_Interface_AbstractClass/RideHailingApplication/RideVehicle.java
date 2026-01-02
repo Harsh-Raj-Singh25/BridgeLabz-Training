8c1a7d8d7a386a1e03d33deb9368ba37dcd74f54
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-01-02 13:09:03 +0530

    [Harsh Raj Singh] : added a new Program showing RideVehicle class in Ride Hailing Application

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/Encapsulation_Polymorphism_Interface_AbstractClass/RideHailingApplication/RideVehicle.java
@@ -0,0 +1,26 @@
+package com.Encapsulation_Polymorphism_Interface_AbstractClass.RideHailingApplication;
+
+//ABSTRACT CLASS: General Vehicle template
+abstract class RideVehicle {
+	// ENCAPSULATION: Sensitive driver/vehicle data hidden
+	private String vehicleId;
+	private String driverName;
+	private double ratePerKm;
+
+	public RideVehicle(String id, String driver, double rate) {
+		this.vehicleId = id;
+		this.driverName = driver;
+		this.ratePerKm = rate;
+	}
+
+	public double getRate() {
+		return ratePerKm;
+	}
+
+	// ABSTRACT METHOD: Fare depends on the vehicle type
+	public abstract double calculateFare(double distance);
+
+	public void getVehicleDetails() {
+		System.out.println("Driver: " + driverName + " [" + vehicleId + "]");
+	}
+}
