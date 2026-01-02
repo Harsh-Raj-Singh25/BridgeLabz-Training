f8882ea8a7d72dfb129044c47b4e68df28fdf622
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-01-02 11:53:41 +0530

    [Harsh Raj Singh] : added a new file denoting interface Insurable in Vehicle Rental System

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/Encapsulation_Polymorphism_Interface_AbstractClass/VehicleRentalSystem/Insurable.java
@@ -0,0 +1,7 @@
+package com.Encapsulation_Polymorphism_Interface_AbstractClass.VehicleRentalSystem;
+
+//Interface for Insurance logic
+interface Insurable {
+	double calculateInsurance();
+	String getInsuranceDetails();
+}
