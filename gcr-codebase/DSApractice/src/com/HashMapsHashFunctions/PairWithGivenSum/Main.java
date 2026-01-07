7e49ff097221eb633833003769d75785b870fd2e
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-01-08 00:01:18 +0530

    [Harsh Raj Singh] : added program files of problem pairs with given sum

---
+++ gcr-codebase/DSApractice/src/com/HashMapsHashFunctions/PairWithGivenSum/Main.java
@@ -0,0 +1,11 @@
+package com.HashMapsHashFunctions.PairWithGivenSum;
+
+public class Main {
+	public static void main(String[] args) {
+
+        int[] arr = {8, 7, 2, 5, 3, 1};
+        int target = 10;
+
+        PairwithgivenSum.hasPair(arr, target);
+    }
+}
