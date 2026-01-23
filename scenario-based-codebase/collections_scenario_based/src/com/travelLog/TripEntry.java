5af723925c85736ac449175aa703c65ac0d7d23a
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-01-23 22:00:00 +0530

    [Harsh Raj Singh] : added program files of travel logger system

---
+++ scenario-based-codebase/collections_scenario_based/src/com/travelLog/TripEntry.java
@@ -0,0 +1,36 @@
+package com.travelLog;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class TripEntry implements Serializable {
+	private static final long serialVersionUID = 1L; // Ensures version compatibility
+	private String country;
+	private List<String> cities;
+	private int duration;
+	private String blogText;
+
+	public TripEntry(String country, List<String> cities, int duration, String blogText) {
+		this.country = country;
+		this.cities = cities;
+		this.duration = duration;
+		this.blogText = blogText;
+	}
+
+	// Getters
+	public String getCountry() {
+		return country;
+	}
+
+	public List<String> getCities() {
+		return cities;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public String getBlogText() {
+		return blogText;
+	}
+}
