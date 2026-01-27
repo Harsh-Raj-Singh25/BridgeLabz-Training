cc521dce77e66e1e79971fe37789e4c81f56a06a
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-01-27 11:50:09 +0530

    [Harsh Raj Singh] : added program files demonstrating working of parameterized test

---
+++ gcr-codebase/collections_practice/src/com/jUnit/parameterizedTests/MathChecker.java
@@ -0,0 +1,7 @@
+package com.jUnit.parameterizedTests;
+
+public class MathChecker {
+	public boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+}
---
+++ gcr-codebase/collections_practice/src/com/jUnit/parameterizedTests/MathCheckerTest.java
@@ -0,0 +1,21 @@
+package com.jUnit.parameterizedTests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathCheckerTest {
+	MathChecker checker = new MathChecker();
+
+	@ParameterizedTest
+	@ValueSource(ints = { 2, 4, 6, 8, 10 })
+	void testIsEvenWithTrueValues(int number) {
+		assertTrue(checker.isEven(number), number + " should be even");
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { 1, 3, 5, 7, 9 })
+	void testIsEvenWithFalseValues(int number) {
+		assertFalse(checker.isEven(number), number + " should be odd");
+	}
+}
