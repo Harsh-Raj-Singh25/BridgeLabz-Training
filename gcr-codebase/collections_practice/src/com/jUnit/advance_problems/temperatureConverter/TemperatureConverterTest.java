b042c3cb698594744f844871af923e360d4a8b7a
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-01-27 12:05:15 +0530

    [Harsh Raj Singh] : added program files for testing temperature converter using JUnit

---
+++ gcr-codebase/collections_practice/src/com/jUnit/advance_problems/temperatureConverter/TemperatureConverterTest.java
@@ -0,0 +1,16 @@
+package com.jUnit.advance_problems.temperatureConverter;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TemperatureConverterTest {
+	TemperatureConverter converter = new TemperatureConverter();
+
+	@Test
+	void testConversions() {
+		// We use a delta of 0.001 to handle floating point precision
+		assertEquals(212.0, converter.celsiusToFahrenheit(100.0), 0.001);
+		assertEquals(32.0, converter.celsiusToFahrenheit(0.0), 0.001);
+		assertEquals(37.0, converter.fahrenheitToCelsius(98.6), 0.001);
+	}
+}
