d634a59c0d3b93a8f97a0ad57780ddc1e981de91
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-02-02 22:46:21 +0530

    [Harsh Raj Singh] : modified program

--- gcr-codebase/java8_practice/src/com/functional_interface/default_methods/payment_gateway/LegacyGateway.java
+++ gcr-codebase/java8_practice/src/com/functional_interface/default_methods/payment_gateway/LegacyGateway.java
@@ -3,7 +3,7 @@ package com.functional_interface.default_methods.payment_gateway;
 class LegacyGateway implements PaymentProcessor {
 	@Override
 	public void processPayment(double amount) {
-		System.out.println("Processing legacy payment of $" + amount);
+		System.out.println("Processing legacy payment of Rs." + amount);
 	}
 	// Uses the default refund() automatically
 
